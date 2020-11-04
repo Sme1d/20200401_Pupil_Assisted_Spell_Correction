efa83dd48b4e3c73560a37852050c1addea154dd
Florian Schmeidl <florianschmeidl@Florians-MacBook-Pro-4.local>
2020-11-04 13:08:48 +0100

    fixed typo

--- constant.py
+++ constant.py
@@ -8,7 +8,7 @@ DWELL_TIME = 700
 PAUSE_TIME = 500
 START_TIME = time.time()
 FILE_NAME = 'testfile.csv'
-PLATTFORM = platform.system()
+PLATFORM = platform.system()
 KEYS = [# Key rows
     ('escape', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '?', '!'),
     ('q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'ü'),
